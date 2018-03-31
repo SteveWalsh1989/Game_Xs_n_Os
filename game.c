0598738bd1cfce166abcd979e771735be6be339a
SteveWalsh1989 <stevewalsh89@gmail.com>
2018-03-31 11:55:14 +0100

    Step 1 Complete: Created  pointer to malloc  structure for game info and printed pointer to console

--- game.c
+++ game.c
@@ -1,4 +1,37 @@
-//
-// Created by Steve on 31/03/2018.
-//
+/******************************************************************************
+ *
+ * C Programming Assignment 2
+ *
+ * Steve Walsh
+ *
+ * R00151053
+ *
+ * 31/3/2018
+ *
+ * Noughts and Crosses Game
+ *
+ *
+ *******************************************************************************/
+//-------------------------------------
+//       Import Statements
+//-------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "game.h"
 
+
+//-------------------------------------
+//        Methods
+//-------------------------------------
+
+void play_game()
+{
+    printf("Xs and Os!\n");                                     //  title for game
+
+    struct game* p_game_info = 0;                               //  create structure for game info
+
+    p_game_info = (struct game * ) malloc(sizeof(struct game)); // create pointer to malloc structure for game info
+
+     printf("Structure pointer is: %p", p_game_info ) ;          // TEST : Prints pointer to structure in console
+}
--- game.h
+++ game.h
@@ -1,24 +1,49 @@
-//
-// Created by Steve on 31/03/2018.
-//
+/******************************************************************************
+ *
+ * C Programming Assignment 2
+ *
+ * Steve Walsh
+ *
+ * R00151053
+ *
+ * 31/3/2018
+ *
+ * Noughts and Crosses Game
+ *
+ *
+ *******************************************************************************/
+
 
 #ifndef NOUGHTS_AND_CROSSES_GAME_GAME_H
 #define NOUGHTS_AND_CROSSES_GAME_GAME_H
 
 
+#define MAX_NAME_LEN 50
+typedef enum Bool boolean;
+
+//-------------------------------------
+//        Lists
+//-------------------------------------
+enum Bool   { False, True };
 
 
 
 
+//-------------------------------------
+//        Structure
+//-------------------------------------
+struct game {
+    char board[3][3];
+    char playerNames[2][MAX_NAME_LEN];
+    int status;
+    boolean finished;
+};
 
+//-------------------------------------
+//        Methods
+//-------------------------------------
 
-
-
-
-
-
-
-
+void play_game();
 
 
 
